e120939a4f6d93b96efaf0adb83d98684654fc9b
박남주 <qkrskawn2@gmail.com>
2023-08-25 13:06:29 +0900

    파일변경2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          파일 변경됨....
+          파일 변경됨22222....
         </p>
         <a
           className="App-link"
